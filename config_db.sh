332c0e00773c7fc02e3261ef2c61f3edf2cbad98
rhamonantunes <66077933+rhamonantunes@users.noreply.github.com>
2024-05-08 21:44:13 -0300

    Update config_db.sh

--- config_db.sh
+++ config_db.sh
@@ -7,7 +7,7 @@ psql -c "CREATE USER mapas"
 # createdb --owner mapas mapas
 psql -d mapas -c "CREATE EXTENSION postgis;"
 echo "Executando config for database $POSTGRES_DB:"
-psql -U mapas -d mapas -c "CREATE EXTENSION unaccent;"
+# psql -U mapas -d mapas -c "CREATE EXTENSION unaccent;"
 psql -U mapas -f /srv/mapas/db/schema.sql
 psql -U mapas -f /srv/mapas/db/initial-data.sql
 
